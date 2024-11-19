7836e11ee507fb32042b246b5f4d5d6eb9a42191
caneregehan <caner.egehan@hotmail.com>
2024-11-19 23:45:03 +0300

    login & register

--- BlogBackend/server.js
+++ BlogBackend/server.js
@@ -81,6 +81,97 @@ app.delete("/blogs/:id", async (req, res) => {
   res.json({ message: "Blog deleted" });
 });
 
+// User Model
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: String,
+    lastname: String,
+    username: String,
+    mail: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema, "user");
+
+// User Routes
+app.get("/user", async (req, res) => {
+  const user = await User.find();
+  res.json(user);
+});
+
+app.get("/user/:id", async (req, res) => {
+  const blog = await User.findById(req.params.id);
+  res.json(blog);
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const { name, lastname, username, mail, password } = req.body;
+
+    if (!mail || !username || !password) {
+      return res.status(400).json({ message: "Eksik alanlar mevcut!" });
+    }
+
+    const newUser = new User({
+      name,
+      lastname,
+      mail,
+      username,
+      password,
+    });
+
+    await newUser.save();
+    res.json(newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.put("/user/:id", async (req, res) => {
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(updatedUser);
+});
+
+app.delete("/user/:id", async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted" });
+});
+
+//login Route
+
+app.post("/user/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ message: "Eksik alanlar mevcut!" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ message: "Kullanıcı bulunamadı!" });
+    }
+
+    if (user.password !== password) {
+      return res.status(400).json({ message: "Yanlış şifre!" });
+    }
+
+    res.json({ status: true, message: "Giriş başarılı!" });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
 });
--- BlogFrontend/src/pages/Register.jsx
+++ BlogFrontend/src/pages/Register.jsx
@@ -1,91 +1,245 @@
-export default function Login() {
-  return (
-    <>
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <div className="w-full p-8 lg:p-36 md:p-52 sm:20 lg:w-1/2">
-          <h1 className="pb-10 text-3xl font-semibold text-center">Kayıt ol</h1>
-          <form action="#" method="POST">
-            <div className="flex gap-2">
-              <div className="flex w-1/2 mb-4">
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Adınız"
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
-                  autoComplete="off"
-                />
-              </div>
-              <div className="flex w-1/2 mb-4">
-                <input
-                  type="text"
-                  id="lastname"
-                  name="lastname"
-                  placeholder="Soyadınız"
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
-                  autoComplete="off"
-                />
-              </div>
-            </div>
+// export default function Register() {
+//   return (
+//     <>
+//       <div className="flex items-center justify-center h-screen bg-gray-100">
+//         <div className="hidden w-1/2 h-screen lg:block">
+//           <img
+//             src="https://w.wallhaven.cc/full/jx/wallhaven-jxgvg5.jpg"
+//             alt="Placeholder Image"
+//             className="object-cover w-full h-full rounded-e-2xl "
+//           />
+//         </div>
+//         <div className="w-full p-8 lg:p-36 md:p-52 sm:20 lg:w-1/2">
+//           <h1 className="pb-10 text-3xl font-semibold text-center">Kayıt ol</h1>
+//           <form action="#" method="POST">
+//             <div className="flex gap-2">
+//               <div className="flex w-1/2 mb-4">
+//                 <input
+//                   type="text"
+//                   id="name"
+//                   name="name"
+//                   placeholder="Adınız"
+//                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+//                   autoComplete="off"
+//                 />
+//               </div>
+//               <div className="flex w-1/2 mb-4">
+//                 <input
+//                   type="text"
+//                   id="lastname"
+//                   name="lastname"
+//                   placeholder="Soyadınız"
+//                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+//                   autoComplete="off"
+//                 />
+//               </div>
+//             </div>
 
-            <div className="mb-4">
+//             <div className="mb-4">
+//               <input
+//                 type="text"
+//                 id="username"
+//                 name="username"
+//                 placeholder="Kullanıcı Adı"
+//                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+//                 autoComplete="off"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <input
+//                 type="mail"
+//                 placeholder="Mail Adresi"
+//                 id="mail"
+//                 name="mail"
+//                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+//                 autoComplete="off"
+//                 pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+//                 required
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <input
+//                 type="password"
+//                 placeholder="Şifre"
+//                 id="password"
+//                 name="password"
+//                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+//                 autoComplete="off"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <input
+//                 type="confirm_password"
+//                 placeholder="Şifre Tekrar"
+//                 id="confirm_password"
+//                 name="confirm_password"
+//                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+//                 autoComplete="off"
+//               />
+//             </div>
+
+//             <button
+//               type="submit"
+//               className="w-full px-4 py-2 font-semibold text-white rounded-md bg-zinc-500 hover:bg-zinc-600">
+//               Kayıt Ol
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const BASE_URL = import.meta.env.VITE_BASE_URL_USER; // Base URL for the backend
+  const navigate = useNavigate(); // React Router hook for navigation
+  const [formData, setFormData] = useState({
+    name: "",
+    lastname: "",
+    username: "",
+    mail: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirm_password) {
+      alert("Şifreler uyuşmuyor!");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${BASE_URL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: formData.name,
+          lastname: formData.lastname,
+          username: formData.username,
+          mail: formData.mail,
+          password: formData.password,
+        }),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        alert("Kayıt başarılı!");
+        navigate("/login");
+        console.log(result);
+      } else {
+        alert("Kayıt sırasında bir hata oluştu.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Bir hata oluştu.");
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="hidden w-1/2 h-screen lg:block">
+        <img
+          src="https://w.wallhaven.cc/full/jx/wallhaven-jxgvg5.jpg"
+          alt="Placeholder Image"
+          className="object-cover w-full h-full rounded-e-2xl "
+        />
+      </div>
+      <div className="w-full p-8 lg:p-36 md:p-52 sm:20 lg:w-1/2">
+        <h1 className="pb-10 text-3xl font-semibold text-center">Kayıt ol</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="flex gap-2">
+            <div className="flex w-1/2 mb-4">
               <input
                 type="text"
-                id="username"
-                name="username"
-                placeholder="Kullanıcı Adı"
+                name="name"
+                placeholder="Adınız"
+                value={formData.name}
+                onChange={handleChange}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
                 autoComplete="off"
               />
             </div>
-            <div className="mb-4">
+            <div className="flex w-1/2 mb-4">
               <input
-                type="mail"
-                placeholder="Mail Adresi"
-                id="mail"
-                name="mail"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
-                autoComplete="off"
-                pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="password"
-                placeholder="Şifre"
-                id="password"
-                name="password"
+                type="text"
+                name="lastname"
+                placeholder="Soyadınız"
+                value={formData.lastname}
+                onChange={handleChange}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
                 autoComplete="off"
               />
             </div>
-            <div className="mb-4">
-              <input
-                type="confirm_password"
-                placeholder="Şifre Tekrar"
-                id="confirm_password"
-                name="confirm_password"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
-                autoComplete="off"
-              />
-            </div>
+          </div>
 
-            <button
-              type="submit"
-              className="w-full px-4 py-2 font-semibold text-white rounded-md bg-zinc-500 hover:bg-zinc-600">
-              Kayıt Ol
-            </button>
-          </form>
-        </div>
-        <div className="hidden w-1/2 h-screen lg:block">
-          <img
-            src="https://w.wallhaven.cc/full/jx/wallhaven-jxgvg5.jpg"
-            alt="Placeholder Image"
-            className="object-cover w-full h-full rounded-e-2xl scale-x-[-1]"
-          />
-        </div>
+          <div className="mb-4">
+            <input
+              type="text"
+              name="username"
+              placeholder="Kullanıcı Adı"
+              value={formData.username}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="email"
+              name="mail"
+              placeholder="Mail Adresi"
+              value={formData.mail}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              name="password"
+              placeholder="Şifre"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              name="confirm_password"
+              placeholder="Şifre Tekrar"
+              value={formData.confirm_password}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-zinc-500"
+              autoComplete="off"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full px-4 py-2 font-semibold text-white rounded-md bg-zinc-500 hover:bg-zinc-600">
+            Kayıt Ol
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
